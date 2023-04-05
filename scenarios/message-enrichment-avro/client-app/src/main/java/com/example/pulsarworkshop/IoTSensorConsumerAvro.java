3c1424868ee5ed5fab0195ea4475ca8446f342f7
yabinmeng <yabinmeng@gmail.com>
2023-04-04 20:25:52 -0500

    1) Add scenario for message enrichment with Avro; 2) Reorganize code structure to make base codes available to all scenarios

---
+++ scenarios/message-enrichment-avro/client-app/src/main/java/com/example/pulsarworkshop/IoTSensorConsumerAvro.java
@@ -0,0 +1,109 @@
+package com.example.pulsarworkshop;
+
+import com.example.pulsarworkshop.pojo.IoTSensorData;
+import com.example.pulsarworkshop.exception.InvalidParamException;
+import com.example.pulsarworkshop.exception.WorkshopRuntimException;
+import org.apache.commons.cli.Option;
+import org.apache.pulsar.client.api.*;
+import org.apache.pulsar.shade.org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IoTSensorConsumerAvro extends PulsarWorkshopCmdApp {
+
+    private final static Logger logger = LoggerFactory.getLogger(IoTSensorConsumerAvro.class);
+
+    private String subscriptionName;
+    private SubscriptionType subscriptionType = SubscriptionType.Exclusive;
+
+    private PulsarClient pulsarClient;
+
+    private Consumer<IoTSensorData> pulsarConsumer;
+
+    public IoTSensorConsumerAvro(String appName, String[] inputParams) {
+        super(appName, inputParams);
+        addCommandLineOption(new Option("sbt","subType", true, "Pulsar subscription type."));
+        addCommandLineOption(new Option("sbn", "subName", true, "Pulsar subscription name."));
+    }
+
+    public static void main(String[] args) {
+        PulsarWorkshopCmdApp workshopApp = new IoTSensorConsumerAvro("IoTSensorConsumerAvro", args);
+        int exitCode = workshopApp.run();
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void processExtendedInputParams() throws InvalidParamException {
+        // (Required) Pulsar subscription name
+        subscriptionName = processStringInputParam("sbn");
+        if ( StringUtils.isBlank(subscriptionName) ) {
+            throw new InvalidParamException("Must provide a subscription name for a consumer!");
+        }
+
+        // (Optional) Pulsar subscription type
+        String subType = processStringInputParam("sbt");
+        if (!StringUtils.isBlank(subType)) {
+        try {
+	            subscriptionType = SubscriptionType.valueOf(subType);
+	        }
+	        catch (IllegalArgumentException iae) {
+	            subscriptionType = SubscriptionType.Exclusive;
+	        }
+        }
+    }
+
+    @Override
+    public void runApp() {
+        try {
+            pulsarClient = createNativePulsarClient();
+
+            ConsumerBuilder<IoTSensorData> consumerBuilder = pulsarClient.newConsumer(Schema.AVRO(IoTSensorData.class));
+            consumerBuilder.topic(pulsarTopicName);
+            consumerBuilder.subscriptionName(subscriptionName);
+            consumerBuilder.subscriptionType(subscriptionType);
+            pulsarConsumer = consumerBuilder.subscribe();
+
+            int msgRecvd = 0;
+            if (numMsg == -1) {
+                numMsg = Integer.MAX_VALUE;
+            }
+
+            while (msgRecvd < numMsg) {
+                Message<IoTSensorData> message = pulsarConsumer.receive();
+                IoTSensorData sensorData = message.getValue();
+
+                if (logger.isDebugEnabled()) {
+                    logger.debug(">>> ({}) Message received and acknowledged: " +
+                                    "msg-key={}; msg-properties={}; msg-payload={}",
+                            pulsarConsumer.getConsumerName(),
+                            message.getKey(),
+                            message.getProperties(),
+                            sensorData);
+                }
+                pulsarConsumer.acknowledge(message);
+
+                msgRecvd++;
+            }
+
+        }
+        catch (PulsarClientException pce) {
+            throw new WorkshopRuntimException("Unexpected error when consuming Pulsar messages: " + pce.getMessage());
+        }
+    }
+
+    @Override
+    public void termApp() {
+        try {
+            if (pulsarConsumer != null) {
+                pulsarConsumer.close();
+            }
+
+            if (pulsarClient != null) {
+                pulsarClient.close();
+            }
+        }
+        catch (PulsarClientException pce) {
+            throw new WorkshopRuntimException("Failed to terminate Pulsar producer or client!");
+        }
+    }
+}
---
+++ scenarios/message-enrichment-avro/client-app/src/main/java/com/example/pulsarworkshop/IoTSensorProducerAvro.java
@@ -0,0 +1,110 @@
+package com.example.pulsarworkshop;
+
+import com.example.pulsarworkshop.pojo.IoTSensorData;
+import com.example.pulsarworkshop.pojo.IoTSensorDataUtils;
+import com.example.pulsarworkshop.exception.InvalidParamException;
+import com.example.pulsarworkshop.exception.WorkshopRuntimException;
+import com.example.pulsarworkshop.util.CsvFileLineScanner;
+import org.apache.commons.cli.Option;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.pulsar.client.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class IoTSensorProducerAvro extends PulsarWorkshopCmdApp {
+
+    private final static Logger logger = LoggerFactory.getLogger(IoTSensorProducerAvro.class);
+
+    private File iotSensorDataCsvFile;
+    private PulsarClient pulsarClient;
+    private Producer<IoTSensorData> pulsarProducer;
+
+    public IoTSensorProducerAvro(String appName, String[] inputParams) {
+        super(appName, inputParams);
+        addCommandLineOption(new Option("csv","csvFile", true, "IoT sensor data CSV file."));
+    }
+
+    public static void main(String[] args) {
+        PulsarWorkshopCmdApp workshopApp = new IoTSensorProducerAvro("IoTSensorProducerAvro", args);
+        int exitCode = workshopApp.run();
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void processExtendedInputParams() throws InvalidParamException {
+        // (Required) CLI option for IoT sensor source file
+        iotSensorDataCsvFile = processFileInputParam("csv");
+        if ( iotSensorDataCsvFile == null) {
+            throw new InvalidParamException("Must provided a valid IoT sensor source data csv file!");
+        }
+    }
+
+    @Override
+    public void runApp() throws WorkshopRuntimException {
+        try {
+            pulsarClient = createNativePulsarClient();
+            ProducerBuilder<IoTSensorData> producerBuilder = pulsarClient.newProducer(Schema.AVRO(IoTSensorData.class));
+            pulsarProducer = producerBuilder.topic(pulsarTopicName).create();
+
+            assert (iotSensorDataCsvFile != null);
+
+            CsvFileLineScanner csvFileLineScanner = new CsvFileLineScanner(iotSensorDataCsvFile);
+            TypedMessageBuilder messageBuilder = pulsarProducer.newMessage();
+
+            boolean isTitleLine = true;
+            String titleLine = "";
+            int msgSent = 0;
+            if (numMsg == -1) {
+                numMsg = Integer.MAX_VALUE;
+            }
+
+            while (csvFileLineScanner.hasNextLine()) {
+                String csvLine = csvFileLineScanner.getNextLine();
+                // Skip the first line which is a title line
+                if (!isTitleLine && StringUtils.isNotBlank(csvLine)) {
+                    if (msgSent < numMsg) {
+                        IoTSensorData data = IoTSensorDataUtils.csvToPojo(csvLine);
+                        MessageId messageId = messageBuilder
+                                .value(data)
+                                .send();
+                        if (logger.isDebugEnabled()) {
+                            logger.debug(">>> Published a message: {}", messageId);
+                        }
+
+                        msgSent++;
+                    } else {
+                        break;
+                    }
+                } else {
+                    isTitleLine = false;
+                    titleLine = csvLine;
+                }
+            }
+
+        } catch (PulsarClientException pce) {
+        	pce.printStackTrace();
+            throw new WorkshopRuntimException("Unexpected error when producing Pulsar messages: " + pce.getMessage());
+        } catch (IOException ioException) {
+            throw new WorkshopRuntimException("Failed to read from the workload data source file: " + ioException.getMessage());
+        }
+    }
+
+    @Override
+    public void termApp() {
+        try {
+            if (pulsarProducer != null) {
+                pulsarProducer.close();
+            }
+
+            if (pulsarClient != null) {
+                pulsarClient.close();
+            }
+        }
+        catch (PulsarClientException pce) {
+            throw new WorkshopRuntimException("Failed to terminate Pulsar producer or client!");
+        }
+    }
+}
