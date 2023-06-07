ae38ef3653503fe77b80ddd5d00f8d01b67c9e67
Patrick Bomar <patrick.bomar@datastax.com>
2023-06-07 13:58:02 -0500

    Add S4R Direct Exchange Demo

--- scenarios/rabbitmq-s4r/direct-exchange/src/main/java/com/example/pulsarworkshop/S4RDirectConsumer.java
+++ scenarios/rabbitmq-s4r/direct-exchange/src/main/java/com/example/pulsarworkshop/S4RDirectConsumer.java
@@ -11,17 +11,17 @@ import com.rabbitmq.client.Envelope;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class S4RTopicConsumer extends S4RCmdApp {
-    private final static String APP_NAME = "S4RTopicConsumer";
+public class S4RDirectConsumer extends S4RCmdApp {
+    private final static String APP_NAME = "S4RDirectConsumer";
     static { System.setProperty("log_file_base_name", getLogFileName(API_TYPE, APP_NAME)); }
-    private final static Logger logger = LoggerFactory.getLogger(S4RTopicConsumer.class);
+    private final static Logger logger = LoggerFactory.getLogger(S4RDirectConsumer.class);
     DefaultConsumer consumer;
-    public S4RTopicConsumer(String appName, String[] inputParams) {
+    public S4RDirectConsumer(String appName, String[] inputParams) {
         super(appName, inputParams);
     }
 
     public static void main(String[] args) {
-        PulsarWorkshopCmdApp workshopApp = new S4RTopicConsumer("S4RTopicConsumer", args);
+        PulsarWorkshopCmdApp workshopApp = new S4RDirectConsumer("S4RDirectConsumer", args);
         int exitCode = workshopApp.runCmdApp();
         System.exit(exitCode);
     }
@@ -40,7 +40,7 @@ public class S4RTopicConsumer extends S4RCmdApp {
             }
             connection = S4RFactory.newConnection();
             channel = connection.createChannel();
-            channel.exchangeDeclare(S4RExchangeName, BuiltinExchangeType.TOPIC);
+            channel.exchangeDeclare(S4RExchangeName, BuiltinExchangeType.DIRECT);
             channel.queueDeclare(S4RQueueName, true, false, false, null);
             channel.queueBind(S4RQueueName, S4RExchangeName, S4RRoutingKey); //Routing key is ignored in "fanout" exchange
             logger.info("Queue name: " + S4RQueueName + " Exchange name: " + S4RExchangeName + " RoutingKey: " + S4RRoutingKey);
